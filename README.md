16ce951695a860f04498dc15e7d21397f7e27cdd
rodrigo-delgado <rod.delgado85@gmail.com>
2017-02-22 17:53:00 +1300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# php-challenge
+
+
+## This is a PHP challenge for a company who asked me to build a simple site using PHP
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>HomePage</title>
+  </head>
+  <body>
+    <h1>User Authentication System</h1>
+  </body>
+</html>
